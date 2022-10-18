b0c0fa94c98f766b55de801b6711fa3c1798542a
nopparat <tomlovetom20111@hotmail.com>
2022-10-18 16:34:32 +0700

    Test

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: [],
+//     theme: {
+//       extend: {
+//         colors: {
+//           "atapy-gray": "#f0f2ef",
+//           "atapy-black": "#232323",
+//           "atapy-zinc": "#e3e3e3",
+//         },
+//         gridTemplateColumns: {
+//           31: "repeat(31, minmax(0, 1fr))",
+//           "4sm": "repeat(4, minmax(8rem,1fr))",
+//           "8sm": "repeat(8, minmax(8rem,1fr))",
+//           "8lg": "repeat(7, minmax(6rem,1fr)) 6fr",
+//         },
+//       },
+//     },
+//     plugins: [],
+//   };
+  
